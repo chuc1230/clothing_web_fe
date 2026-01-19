cff43bafd4952e67498e067e57ad2f74b83ca9bc
chuc <minhchuc.hhck2003@gmail.com>
2026-01-19 16:06:24 +0700

    19/1

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,7 +3,6 @@ import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
@@ -14,7 +13,6 @@ const Product = () => {
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product}/>
-      <DescriptionBox />
       <RelatedProducts product={product}/>
     </div>
   )
